4638c1071f934ef39dbb20a569c4b32f5330222b
JoelKanyi <joelkanyi98@gmail.com>
2020-05-31 10:59:26 +0300

    added listener to the bottomnav

--- app/src/main/java/com/example/clusterpointscalculator/FactsFragment.kt
+++ app/src/main/java/com/example/clusterpointscalculator/FactsFragment.kt
@@ -1,17 +1,79 @@
 package com.example.clusterpointscalculator
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.home_fragment.*
 
-class FactsFragment :Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+class FactsFragment : Fragment(){
+
+    val TAG = "FactsFragment"
+
+    override fun onAttach(context: Context) {
+        Log.d(TAG,"onAttach")
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG,"onCreate")
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        Log.d(TAG,"onCreateView")
+        //!! means the inflater should not be null
+        return inflater!!.inflate(R.layout.facts_fragment,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        Log.d(TAG,"onActivityCreated")
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onStart() {
+        Log.d(TAG,"onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(TAG,"onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG,"onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG,"onStop")
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        Log.d(TAG,"onDestroyView")
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG,"onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        Log.d(TAG,"onDetach")
+        super.onDetach()
+    }
+
+    fun calculatePoints(){
+        var subj1 = clusterSubj1.text
+        var subj2 = clusterSubj2.text
+        var subj3 = clusterSubj3.text
+        var subj4 = clusterSubj4.text
+
     }
 }
--- app/src/main/java/com/example/clusterpointscalculator/MainActivity.kt
+++ app/src/main/java/com/example/clusterpointscalculator/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.clusterpointscalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        val fragment = HomeFragment()
+        addFragment(fragment)
+    }
+
+
+    fun addFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container,fragment)
+            .commit()
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when(item.itemId){
+            R.id.bottom_navigation_home ->{
+                val fragment = HomeFragment()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.bottom_navigation_facts ->{
+                val fragment = FactsFragment()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
     }
 }
