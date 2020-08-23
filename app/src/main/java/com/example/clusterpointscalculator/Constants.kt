2753d569512a1be503fe9e83b26019644222cc5d
JoelKanyi <joelkanyi98@gmail.com>
2020-08-23 19:30:23 +0300

    added constants

---
+++ app/src/main/java/com/example/clusterpointscalculator/Constants.kt
@@ -0,0 +1,15 @@
+package com.example.clusterpointscalculator
+
+const val PREFERENCE_ENGLISH = "com.example.clusterpointscalculator"
+const val PREFERENCE_KISWAHILI= "com.example.clusterpointscalculator"
+const val PREFERENCE_MATHEMATICS= "com.example.clusterpointscalculator"
+const val PREFERENCE_PHYSICS= "com.example.clusterpointscalculator"
+const val PREFERENCE_BIOLOGY= "com.example.clusterpointscalculator"
+const val PREFERENCE_CHEMISTRY= "com.example.clusterpointscalculator"
+const val PREFERENCE_AGRICULTURE= "com.example.clusterpointscalculator"
+const val PREFERENCE_BUSINESS= "com.example.clusterpointscalculator"
+const val PREFERENCE_COMPUTER= "com.example.clusterpointscalculator"
+const val PREFERENCE_GEOGRAPHY= "com.example.clusterpointscalculator"
+const val PREFERENCE_HISTORY= "com.example.clusterpointscalculator"
+const val PREFERENCE_CRE= "com.example.clusterpointscalculator"
+
--- app/src/main/java/com/example/clusterpointscalculator/Main.kt
+++ app/src/main/java/com/example/clusterpointscalculator/Main.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.main_layout.*
+import com.example.clusterpointscalculator.PreferenceHelper.set
 
 class Main : AppCompatActivity() {
 
@@ -13,5 +15,17 @@ class Main : AppCompatActivity() {
 
         val prefs: SharedPreferences = PreferenceHelper.defaultPrefs(applicationContext!!)
 
+        prefs[PREFERENCE_ENGLISH] = pref_Eng.text
+        prefs[PREFERENCE_KISWAHILI] = pref_Kisw.text
+        prefs[PREFERENCE_MATHEMATICS] = pref_Maths.text
+        prefs[PREFERENCE_PHYSICS] = pref_Phyc.text
+        prefs[PREFERENCE_BIOLOGY] = pref_Bio.text
+        prefs[PREFERENCE_CHEMISTRY] = pref_Chem.text
+        prefs[PREFERENCE_AGRICULTURE] = pref_Bst.text
+        prefs[PREFERENCE_BUSINESS] = pref_Agri.text
+        prefs[PREFERENCE_COMPUTER] = pref_Comp.text
+        prefs[PREFERENCE_GEOGRAPHY] = pref_Hist.text
+        prefs[PREFERENCE_HISTORY] = pref_Geo.text
+        prefs[PREFERENCE_CRE] = pref_CRE.text
     }
 }
